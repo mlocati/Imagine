01bebe596f5e856a9cb5b6af075d7a3163ea7173
Michele Locati <michele@locati.it>
2018-09-05 08:04:53 +0200

    Temp

--- tests/Imagine/Test/Draw/AbstractDrawerTest.php
+++ tests/Imagine/Test/Draw/AbstractDrawerTest.php
@@ -20,6 +20,202 @@ use Imagine\Test\ImagineTestCase;
 
 abstract class AbstractDrawerTest extends ImagineTestCase
 {
+    public function thicknessProvider()
+    {
+        return array(
+            array(0),
+            array(1),
+            array(4),
+        );
+    }
+
+    public function thicknessAndFillProvider()
+    {
+        $result = array();
+        foreach ($this->thicknessProvider() as $thicknessData) {
+            $result[] = array_merge($thicknessData, array(false));
+            $result[] = array_merge($thicknessData, array(true));
+        }
+
+        return $result;
+    }
+
+    /**
+     * * @dataProvider thicknessProvider
+     *
+     * @param int $thickness
+     */
+    public function testArc($thickness)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(40, 30), $this->getColor('fff'));
+        $size = $image->getSize();
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->arc(new Center($size), $size->scale(0.5), 0, 180, $this->getColor('f00')));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/arc/thinkness{$thickness}.png"), $image, '', 0.134);
+    }
+
+    /**
+     * * @dataProvider thicknessAndFillProvider
+     *
+     * @param int $thickness
+     * @param bool $fill
+     */
+    public function testChord($thickness, $fill)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(60, 50), $this->getColor('fff'));
+        if ($image instanceof \Imagine\Gd\Image && $fill) {
+            $this->markTestSkipped('The GD Drawer can NOT draw correctly filled chords');
+        }
+        $fill01 = $fill ? 1 : 0;
+        $size = $image->getSize();
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->chord(new Center($size), $size->scale(0.8), 0, 240, $this->getColor('f00'), $fill, $thickness));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/chord/thinkness{$thickness}-fill{$fill01}.png"), $image, '', 0.153);
+    }
+
+    /**
+     * @dataProvider thicknessAndFillProvider
+     *
+     * @param bool $fill
+     * @param int $thickness
+     */
+    public function testCircle($thickness, $fill)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(20, 20), $this->getColor('fff'));
+        if ($image instanceof \Imagine\Gd\Image && !$fill && $thickness > 1) {
+            $this->markTestSkipped('The GD Drawer can NOT draw correctly not filled circles with a thickness greater than 1');
+        }
+
+        $size = $image->getSize();
+        $fill01 = $fill ? 1 : 0;
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->circle(new Center($size), min($size->getWidth(), $size->getHeight()) * 0.9, $this->getColor('f00'), $fill, $thickness));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/circle/thinkness{$thickness}-fill{$fill01}.png"), $image, '', 0.55);
+    }
+
+    /**
+     * @dataProvider thicknessAndFillProvider
+     *
+     * @param bool $fill
+     * @param int $thickness
+     */
+    public function testEllipse($thickness, $fill)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(30, 20), $this->getColor('fff'));
+        if ($image instanceof \Imagine\Gd\Image && !$fill && $thickness > 1) {
+            $this->markTestSkipped('The GD Drawer can NOT draw correctly not filled ellipses with a thickness greater than 1');
+        }
+
+        $size = $image->getSize();
+        $fill01 = $fill ? 1 : 0;
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->ellipse(new Center($size), $size->scale(0.9), $this->getColor('f00'), $fill, $thickness));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/ellipse/thinkness{$thickness}-fill{$fill01}.png"), $image, '', 0.434);
+    }
+
+    /**
+     * @dataProvider thicknessProvider
+     *
+     * @param bool $fill
+     * @param mixed $thickness
+     */
+    public function testLine($thickness)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(30, 20), $this->getColor('fff'));
+        $size = $image->getSize();
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->line(new Point(5, 5), new Point($size->getWidth() - 5, $size->getHeight() - 6), $this->getColor('f00'), $thickness));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/line/thinkness{$thickness}.png"), $image, '', 0.09);
+    }
+
+    /**
+     * @dataProvider thicknessAndFillProvider
+     *
+     * @param bool $fill
+     * @param int $thickness
+     */
+    public function testPieSlice($thickness, $fill)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(40, 40), $this->getColor('fff'));
+        $size = $image->getSize();
+        $fill01 = $fill ? 1 : 0;
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->pieSlice(new Point($size->getWidth() / 2, 5), $size->scale(0.9), 45, 135, $this->getColor('f00'), $fill, $thickness));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/pieslice/thinkness{$thickness}-fill{$fill01}.png"), $image, '', 0.095);
+    }
+
+    public function testDot()
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(3, 3), $this->getColor('fff'));
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->dot(new Point(1, 1), $this->getColor('f00')));
+        $this->assertImageEquals($imagine->open('tests/Imagine/Fixtures/drawer/dot/dot.png'), $image, '', 0.095);
+    }
+
+    /**
+     * @dataProvider thicknessAndFillProvider
+     *
+     * @param bool $fill
+     * @param int $thickness
+     */
+    public function testRectangle($thickness, $fill)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(20, 25), $this->getColor('fff'));
+        $size = $image->getSize();
+        $fill01 = $fill ? 1 : 0;
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->rectangle(new Point(5, 5), new Point($size->getWidth() - 5, $size->getHeight() - 5), $this->getColor('f00'), $fill, $thickness));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/rectangle/thinkness{$thickness}-fill{$fill01}.png"), $image, '', 0.12);
+    }
+
+    /**
+     * @dataProvider thicknessAndFillProvider
+     *
+     * @param bool $fill
+     * @param int $thickness
+     */
+    public function testPolygon($thickness, $fill)
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(25, 25), $this->getColor('fff'));
+        $size = $image->getSize();
+        $fill01 = $fill ? 1 : 0;
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->polygon(
+            array(
+                new Point($size->getWidth() / 2, 5),
+                new Point($size->getWidth() - 5, $size->getHeight() - 5),
+                new Point(5, $size->getHeight() - 5),
+            ),
+            $this->getColor('f00'),
+            $fill,
+            $thickness
+        ));
+        $this->assertImageEquals($imagine->open("tests/Imagine/Fixtures/drawer/polygon/thinkness{$thickness}-fill{$fill01}.png"), $image, '', 0.154);
+    }
+
+    public function testText()
+    {
+        $imagine = $this->getImagine();
+        $image = $imagine->create(new Box(60, 60), $this->getColor('fff'));
+        $drawer = $image->draw();
+        $this->assertSame($drawer, $drawer->text(
+            'test',
+            $imagine->font('tests/Imagine/Fixtures/font/Arial.ttf', 12, $this->getColor('f00')),
+            new Point(3, 3),
+            45
+        ));
+        $this->assertImageEquals($imagine->open('tests/Imagine/Fixtures/drawer/text/text.png'), $image, '', 0.049);
+    }
+
     public function testDrawASmileyFace()
     {
         $imagine = $this->getImagine();
@@ -36,113 +232,6 @@ abstract class AbstractDrawerTest extends ImagineTestCase
         $this->assertFileExists('tests/Imagine/Fixtures/smiley.png');
 
         unlink('tests/Imagine/Fixtures/smiley.png');
-    }
-
-    public function drawACircleProvider()
-    {
-        return array(
-            array(false, 1),
-            array(true, 1),
-            array(false, 2),
-            array(true, 2),
-        );
-    }
-
-    /**
-     * @dataProvider drawACircleProvider
-     *
-     * @param bool $fill
-     * @param int $thickness
-     */
-    public function testDrawACircle($fill, $thickness)
-    {
-        $imagine = $this->getImagine();
-        $image = $imagine->create(new Box(15, 15), $this->getColor('fff'));
-        $drawer = $image->draw();
-        $this->assertSame($drawer, $drawer->circle(
-            new Point(7, 7),
-            6,
-            $this->getColor('f00'),
-            $fill,
-            $thickness
-        ));
-        $expected = $imagine->open('tests/Imagine/Fixtures/drawer/circle/thinkness' . $thickness . '-filled' . ($fill ? '1' : '0') . '.png');
-        $this->assertImageEquals($expected, $image, '', 0.107);
-    }
-
-    public function testDrawAnEllipse()
-    {
-        $imagine = $this->getImagine();
-
-        $canvas = $imagine->create(new Box(400, 300), $this->getColor('000'));
-
-        $canvas->draw()
-            ->ellipse(new Center($canvas->getSize()), new Box(300, 200), $this->getColor('fff'), true);
-
-        $canvas->save('tests/Imagine/Fixtures/ellipse.png');
-
-        $this->assertFileExists('tests/Imagine/Fixtures/ellipse.png');
-
-        unlink('tests/Imagine/Fixtures/ellipse.png');
-    }
-
-    public function testDrawAPieSlice()
-    {
-        $imagine = $this->getImagine();
-
-        $canvas = $imagine->create(new Box(400, 300), $this->getColor('000'));
-
-        $canvas->draw()
-            ->pieSlice(new Point(200, 150), new Box(100, 200), 45, 135, $this->getColor('fff'), true);
-
-        $canvas->save('tests/Imagine/Fixtures/pie.png');
-
-        $this->assertFileExists('tests/Imagine/Fixtures/pie.png');
-
-        unlink('tests/Imagine/Fixtures/pie.png');
-    }
-
-    public function testDrawAChord()
-    {
-        $imagine = $this->getImagine();
-
-        $canvas = $imagine->create(new Box(400, 300), $this->getColor('000'));
-
-        $canvas->draw()
-            ->chord(new Point(200, 150), new Box(100, 200), 45, 135, $this->getColor('fff'), true);
-
-        $canvas->save('tests/Imagine/Fixtures/chord.png');
-
-        $this->assertFileExists('tests/Imagine/Fixtures/chord.png');
-
-        unlink('tests/Imagine/Fixtures/chord.png');
-    }
-
-    public function testDrawALine()
-    {
-        $imagine = $this->getImagine();
-
-        $canvas = $imagine->create(new Box(400, 300), $this->getColor('000'));
-
-        $canvas->draw()
-            ->line(new Point(50, 50), new Point(350, 250), $this->getColor('fff'))
-            ->line(new Point(50, 250), new Point(350, 50), $this->getColor('fff'));
-
-        $canvas->save('tests/Imagine/Fixtures/lines.png');
-
-        $this->assertFileExists('tests/Imagine/Fixtures/lines.png');
-
-        unlink('tests/Imagine/Fixtures/lines.png');
-    }
-
-    public function drawARectangleProvider()
-    {
-        return array(
-            array(false, 1),
-            array(true, 1),
-            array(false, 2),
-            array(true, 2),
-        );
     }
 
     /**
@@ -205,23 +294,6 @@ abstract class AbstractDrawerTest extends ImagineTestCase
         $this->assertFileExists('tests/Imagine/Fixtures/dot.png');
 
         unlink('tests/Imagine/Fixtures/dot.png');
-    }
-
-    public function testDrawAnArc()
-    {
-        $imagine = $this->getImagine();
-
-        $canvas = $imagine->create(new Box(400, 300), $this->getColor('000'));
-        $size = $canvas->getSize();
-
-        $canvas->draw()
-            ->arc(new Center($size), $size->scale(0.5), 0, 180, $this->getColor('fff'));
-
-        $canvas->save('tests/Imagine/Fixtures/arc.png');
-
-        $this->assertFileExists('tests/Imagine/Fixtures/arc.png');
-
-        unlink('tests/Imagine/Fixtures/arc.png');
     }
 
     public function testDrawText()
